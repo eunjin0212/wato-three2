169a5e6616ff2a6f1266cee40befb5fc254aeafc
eunjin0212 <eunjin9639@gmail.com>
2024-01-01 21:46:12 +0900

    ✨ 화면 잠금 설정 페이지 api 연결

--- src/components/ui/Toggle.jsx
+++ src/components/ui/Toggle.jsx
@@ -1,38 +1,22 @@
-import { useState } from "react";
 
-const Toggle = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
-
+const Toggle = ({value, onChange}) => {
   return (
-    <>
-      <label className="autoSaverSwitch relative inline-flex cursor-pointer select-none items-center">
-        <input
-          type="checkbox"
-          name="autoSaver"
-          className="sr-only"
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-        />
+    <label className='autoSaverSwitch relative inline-flex cursor-pointer select-none items-center'>
+      <input
+        type='checkbox'
+        name='autoSaver'
+        className='sr-only'
+        checked={value}
+        onChange={onChange}
+      />
+      <span
+        className={['slider mr-3 flex h-[32px] w-[56px] items-center rounded-full p-1 duration-200 ', value ? 'bg-primary' : 'bg-[#CCCCCE]'].join(' ')}
+      >
         <span
-          className={`slider mr-3 flex h-[32px] w-[56px] items-center rounded-full p-1 duration-200 ${
-            isChecked ? "bg-primary" : "bg-[#CCCCCE]"
-          }`}
-        >
-          <span
-            className={`dot h-[24px] w-[24px] rounded-full bg-white duration-200 ${
-              isChecked ? "translate-x-6" : ""
-            }`}
-          ></span>
-        </span>
-        {/* <span className="label flex items-center text-sm font-medium text-black">
-          Auto Saver <span className="pl-1"> {isChecked ? "On" : "Off"} </span>
-        </span> */}
-      </label>
-    </>
+          className={['dot h-[24px] w-[24px] rounded-full bg-white duration-200', value && 'translate-x-6'].join(' ')}
+        ></span>
+      </span>
+    </label>
   );
 };
 
--- src/routes/Lock.jsx
+++ src/routes/Lock.jsx
@@ -1,52 +1,78 @@
 
-import Menu from "@/components/Menu";
-import Topbar from "@/ui/Topbar";
-import Toggle from "@/ui/Toggle";
+import Menu from '@/components/Menu';
+import Topbar from '@/ui/Topbar';
+import Toggle from '@/ui/Toggle';
+import { api } from '@/api/axios';
+import lockIcon from '@/assets/ico-lock.png';
+import { useEffect, useState } from 'react';
 
-import lockIcon from "@/assets/ico-lock.png";
-
-const documents = [
-  {
-    title: "화면 잠그기",
-  },
-];
-
-function Item({ title }) {
+function Item({ title, value, onChange }) {
   return (
-    <div className="p-4 rounded-lg relative flex justify-between items-center">
-      <h2 className="text-xl font-medium">{title}</h2>
-      <div className="flex items-center">
-        <label className="flex cursor-pointer select-none items-center">
-          <Toggle />
+    <div className='p-4 rounded-lg relative flex justify-between items-center'>
+      <h2 className='text-xl font-medium'>{title}</h2>
+      <div className='flex items-center'>
+        <label className='flex cursor-pointer select-none items-center'>
+          <Toggle value={value} onChange={onChange} />
         </label>
       </div>
-      <div className="absolute top-0 left-0 right-0 border-t border-gray-300"></div>
-      <div className="absolute bottom-0 left-0 right-0 border-b border-gray-300 mt-4"></div>
+      <div className='absolute top-0 left-0 right-0 border-t border-gray-300'></div>
+      <div className='absolute bottom-0 left-0 right-0 border-b border-gray-300 mt-4'></div>
     </div>
   );
 }
 
-export default function Lock() {
+const Lock = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  /**
+ * @description 화면 잠금 설정
+ */
+  async function getScreenLock() {
+    try {
+      const res = await api.get('')
+      setIsChecked(res.data.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getScreenLock()
+  }, [])
+
+  /**
+   * @param {React.ChangeEvent<HTMLInputElement>} event
+   * @description 화면 잠금 api
+   */
+  async function handleScreenLock(event) {
+    setIsChecked(event.target.value)
+    try {
+      const res = await api.put('')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <div className="flex flex-col lg:flex-row bg-gray-100 min-h-screen">
-      <div className="flex-1">
-        <Topbar title={"화면 잠금 설정"} />
-        <div className="max-w-3xl mx-auto py-10 flex">
+    <div className='flex flex-col lg:flex-row bg-gray-100 min-h-screen'>
+      <div className='flex-1'>
+        <Topbar title='화면 잠금 설정' />
+        <div className='max-w-3xl mx-auto py-10 flex'>
           <Menu />
-          <div className="flex-1 ml-5 mr-5 lg:ml-8 lg:mr-8">
-            <div className="flex flex-col  items-center lg:px-20 text-center">
-              <img src={lockIcon} alt="Alert Icon" />
-              <h2 className="text-xl font-regular py-4">
+          <div className='flex-1 ml-5 mr-5 lg:ml-8 lg:mr-8'>
+            <div className='flex flex-col  items-center lg:px-20 text-center'>
+              <img src={lockIcon} alt='Alert Icon' />
+              <h2 className='text-xl font-regular py-4'>
                 화면 잠금시 앱을 실행할 때마다 비밀번호를 <br />
                 확인하며 보다 안전하게 서비스를 이용할 수 있습니다.
               </h2>
             </div>
-            {documents.map((doc, index) => (
-              <Item key={index} {...doc} />
-            ))}
+            <Item title='화면 잠그기' value={isChecked} onChange={handleScreenLock} />
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+export default Lock
