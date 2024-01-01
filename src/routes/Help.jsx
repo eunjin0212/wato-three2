eb0917e5de5a36ab8078353954ff9e0ef98e1242
eunjin0212 <eunjin9639@gmail.com>
2024-01-01 21:16:40 +0900

    ✨ 도움말 페이지 api 연결

--- src/routes/Help.jsx
+++ src/routes/Help.jsx
@@ -1,45 +1,41 @@
 import Menu from "@/components/Menu";
 import Topbar from "@/ui/Topbar";
 import Dropdown from "@/ui/Dropdown";
+import { api } from '@/api/axios';
+import { useEffect, useState } from 'react';
 
-const documents = [
-  {
-    title: '관심키워드 “전자제품"의 새로운 도움말',
-    date: "2023-10-10",
-    description: "공지 : 정기 점검 및 각종 시스템 업데이트 상황 정보",
-    detail: "이벤트: 이벤트 정보",
-    note: "필독 : 주요 서비스 업데이트 및 보안 관련 정보",
-  },
-  {
-    title: '관심키워드 “전자제품"의 새로운 도움말',
-    date: "2023-10-10",
-    description: "공지 : 정기 점검 및 각종 시스템 업데이트 상황 정보",
-    detail: "이벤트: 이벤트 정보",
-    note: "필독 : 주요 서비스 업데이트 및 보안 관련 정보",
-  },
-  {
-    title: '관심키워드 “전자제품"의 새로운 도움말',
-    date: "2023-10-10",
-    description: "공지 : 정기 점검 및 각종 시스템 업데이트 상황 정보",
-    detail: "이벤트: 이벤트 정보",
-    note: "필독 : 주요 서비스 업데이트 및 보안 관련 정보",
-  },
-];
+const Help = () => {
+  const [documents, setDocuments] = useState([])
 
-export default function Help() {
+  async function getHelp() {
+    try {
+      const res = await api.get('faq')
+      setDocuments(res.data.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getHelp()
+  }, [])
   return (
     <div className="flex flex-col lg:flex-row bg-gray-100 min-h-screen">
       <div className="flex-1">
-        <Topbar title={"공지사항"} />
+        <Topbar title='도움말' />
         <div className="max-w-3xl mx-auto py-10 flex">
           <Menu />
           <div className="flex-1 ml-5 mr-5 lg:ml-8 lg:mr-8">
-            {documents.map((doc, index) => (
-              <Dropdown key={index} {...doc} />
-            ))}
+            {
+              documents.length ? documents.map((doc, index) => (
+                <Dropdown key={index} {...doc} />
+              )) : '도움말이 없습니다.'
+            }
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+export default Help
