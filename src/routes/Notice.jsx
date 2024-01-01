e09161b2fb895bec8f62bb4cff65578352a517f3
eunjin0212 <eunjin9639@gmail.com>
2024-01-01 21:16:45 +0900

    ✨ 공지사항 페이지 api 연결

--- src/routes/Notice.jsx
+++ src/routes/Notice.jsx
@@ -1,34 +1,26 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Menu from "@/components/Menu";
 import Topbar from "@/ui/Topbar";
 import Dropdown from "@/ui/Dropdown";
+import { api } from '@/api/axios';
 
-const documents = [
-  {
-    title: '관심키워드 “전자제품"의 새로운 소식이 올라왔어요',
-    date: "2023-10-10",
-    description: "공지 : 정기 점검 및 각종 시스템 업데이트 상황 정보",
-    detail: "이벤트: 이벤트 정보",
-    note: "필독 : 주요 서비스 업데이트 및 보안 관련 정보",
-  },
-  {
-    title: '관심키워드 “전자제품"의 새로운 소식이 올라왔어요',
-    date: "2023-10-10",
-    description: "공지 : 정기 점검 및 각종 시스템 업데이트 상황 정보",
-    detail: "이벤트: 이벤트 정보",
-    note: "필독 : 주요 서비스 업데이트 및 보안 관련 정보",
-  },
-  {
-    title: '관심키워드 “전자제품"의 새로운 소식이 올라왔어요',
-    date: "2023-10-10",
-    description: "공지 : 정기 점검 및 각종 시스템 업데이트 상황 정보",
-    detail: "이벤트: 이벤트 정보",
-    note: "필독 : 주요 서비스 업데이트 및 보안 관련 정보",
-  },
-];
+const Notice = () => {
+  const [documents, setDocuments] = useState([])
 
-export default function Notice() {
+  async function getNotice() {
+    try {
+      const res = await api.get('announcement')
+      setDocuments(res.data.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  useEffect(() => {
+    getNotice()
+  }, [])
+
   return (
     <div className="flex flex-col lg:flex-row bg-gray-100 min-h-screen">
       <div className="flex-1">
@@ -36,12 +28,16 @@ export default function Notice() {
         <div className="max-w-3xl mx-auto py-10 flex">
           <Menu />
           <div className="flex-1 ml-5 mr-5 lg:ml-8 lg:mr-8">
-            {documents.map((doc, index) => (
-              <Dropdown key={index} {...doc} />
-            ))}
+            {
+              documents.length ? documents.map((doc, index) => (
+                <Dropdown key={index} {...doc} />
+              )) : '공지사항이 없습니다.'
+            }
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+export default Notice
