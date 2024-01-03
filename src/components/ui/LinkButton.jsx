ce72d0044800c8650bf40afdfc765b3afa8588e5
eunjin0212 <eunjin9639@gmail.com>
2024-01-03 21:42:06 +0900

    💄 startbtn 퍼블리싱

--- src/components/ui/LinkButton.jsx
+++ src/components/ui/LinkButton.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const LinkButton = ({ label, to }) => {
   return (
-    <Link to={to} className='button lg:w-96 w-80'>
+    <Link to={to} className='button lg:w-96 max-w-[20rem] w-full flex-1'>
       {label}
     </Link>
   )
