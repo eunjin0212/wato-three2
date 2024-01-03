295ac7e1845a0b34e45ee095cb9dfdd9b82b703b
eunjin0212 <eunjin9639@gmail.com>
2024-01-03 21:41:46 +0900

    💄 startbtn 퍼블리싱

--- src/routes/Started.jsx
+++ src/routes/Started.jsx
@@ -20,20 +20,17 @@ export default function Started() {
     Cookies.remove('token')
   }, [])
   return (
-    <div className="flex flex-col lg:flex-row bg-primary min-h-screen">
-      <div className="flex-1 px-10 flex flex-col text-center justify-between items-center">
+    <main className='relative flex flex-col lg:flex-row bg-primary min-h-screen before:absolute before:-z-10 before:content-[""] before:top-0 before:left-0 before:w-full before:h-full before:bg-[url("/img")]'>
+      <section className="flex-1 px-10 flex flex-col text-center justify-between items-center">
         <img src={logo} alt="Home Icon" className="mt-40 mb-4 h-30" />
 
-        <div className="mb-40 lg:mb-0">
+        <div className="mb-40 lg:mb-0 flex flex-col px-2 w-full lg:items-center">
           <h1 className="font-medium text-white text-xl  mt-10 mb-2">
             &quot;우리는 하나&quot;
           </h1>
-
-          <div className="flex flex-col">
-            {links.map(({ label, to }) => (
-              <LinkButton key={label} to={to} label={label} />
-            ))}
-          </div>
+          {links.map(({ label, to }) => (
+            <LinkButton key={label} to={to} label={label} />
+          ))}
         </div>
 
         <img
@@ -41,12 +38,12 @@ export default function Started() {
           alt="Desktop Background"
           className="w-auto hidden lg:block"
         />
-      </div>
+      </section>
       <img
         src={bgMobile}
         alt="Mobile Background"
         className="w-full block lg:hidden"
       />
-    </div>
+    </main>
   );
 }
